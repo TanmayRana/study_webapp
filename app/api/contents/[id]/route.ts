034bb25a546b24eed0327bad5420408377c07445
Tanmay Rana <tanmayrana2001@gmail.com>
2025-08-13 16:12:59 +0530

    3rd add

--- app/api/contents/[id]/route.ts
+++ app/api/contents/[id]/route.ts
@@ -3,7 +3,10 @@ import { connectDB } from "@/lib/mongodb";
 import { Content } from "@/lib/Schemas";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   await connectDB();
 
   try {

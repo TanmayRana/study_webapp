06e4bd145aec080fadf48f01b53b85a42c051897
Tanmay Rana <tanmayrana2001@gmail.com>
2025-08-13 16:42:14 +0530

    5th add

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,17 +2,10 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import DashboardProvider from "@/app/_components/DashboardProvider";
-// import DashboardProvider from "@/app/_components/DashboardProvider";
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return (
-    <NextThemesProvider {...props}>
-      {/* <DashboardProvider>{children}</DashboardProvider> */}
-      {children}
-    </NextThemesProvider>
-  );
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
